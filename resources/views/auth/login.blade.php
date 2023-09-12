019d55925ba160523d4180da9ee8e5b1dd93b35a
williamaquinojda <144297549+williamaquinojda@users.noreply.github.com>
2023-09-12 12:02:44 -0300

    Teste deploy

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
         <div>
             <x-input-label for="email" :value="__('E-mail')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
-                autofocus autocomplete="username" />
+                autofocus autocomplete="username" />xxxxxxxxxxx
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
